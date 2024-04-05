8bcbb8e091a0afb18bd2cfd4fb0fa980d2450b69
maboelkheir-test1 <mohamed.osama.aboelkheir+test1@gmail.com>
2024-04-05 15:09:57 -0400

    * re-introduced the vulnerability

--- app.js
+++ app.js
@@ -57,8 +57,9 @@ app.use(function(err, req, res, next) {
 
 app.get('/vulnerable', (req, res) => {
  if (req.query.url) {
- 	//res.redirect(req.query.url);
+
 	console.log(req.query.url);
+ 	res.redirect(req.query.url);
  } else {
 	 res.redirect('https://www.example.com');
  }
