7d770d96f703fe5ab9a3d82e967e2eba0eaac5e8
Mohamed AboElKheir <mohamed.aboelkheir@ironcladhq.com>
2024-04-14 22:56:11 -0400

    added another vulnerable route for testing

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/vulnerable', (req, res) => {
  if (req.query.url) {
 
 	console.log(req.query.url);
- 	//res.redirect(req.query.url);
+ 	res.redirect(req.query.url);
  } else {
 	 res.redirect('https://www.example.com');
  }
